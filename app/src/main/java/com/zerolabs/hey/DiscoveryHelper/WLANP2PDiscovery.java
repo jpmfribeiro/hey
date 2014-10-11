dda175fb508b7914cdc01c505b04856658226ff1
Marc Heim <heimm@ethz.ch>
2014-10-11 06:02:52 +0200

    Added WLANP2PDiscovery class

---
+++ app/src/main/java/com/zerolabs/hey/DiscoveryHelper/WLANP2PDiscovery.java
@@ -0,0 +1,78 @@
+package com.zerolabs.hey.DiscoveryHelper;
+
+import android.content.Context;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.util.Log;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by march_000 on 11.10.2014.
+ */
+public class WLANP2PDiscovery {
+    WifiP2pManager mWifiP2pManager;
+    WifiP2pManager.Channel mChannel;
+    MACListener cMacListener;
+    Context mContext;
+
+    public WLANP2PDiscovery(Context context){
+        mWifiP2pManager = (WifiP2pManager) context.getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mWifiP2pManager.initialize(context, context.getMainLooper(), null);
+        mContext = context;
+    }
+
+
+    public void Initialize(){
+        mWifiP2pManager.initialize(mContext, mContext.getMainLooper(), new WifiP2pManager.ChannelListener() {
+            @Override
+            public void onChannelDisconnected() {
+                //TODO
+            }
+        });
+    }
+
+
+    public void discoverMACAdresses(MACListener macListener){
+        cMacListener = macListener;
+        mWifiP2pManager.discoverPeers(mChannel, mPeerToMACListener);
+    }
+
+    PeerToMACListener mPeerToMACListener = new PeerToMACListener();
+    class PeerToMACListener implements WifiP2pManager.ActionListener {
+
+        @Override
+        public void onSuccess() {
+            mWifiP2pManager.requestPeers(mChannel, mPeerListToMACListener);
+        }
+
+        @Override
+        public void onFailure(int i) {
+            Log.v("MAC Address: ", "failure");
+        }
+    }
+
+    PeerListToMACListener mPeerListToMACListener = new PeerListToMACListener();
+    class PeerListToMACListener implements WifiP2pManager.PeerListListener{
+        @Override
+        public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
+            Collection<WifiP2pDevice> wifiP2pDeviceCollection = wifiP2pDeviceList.getDeviceList();
+
+            List<String> returnMACList = new LinkedList<String>();
+            Log.v("MAC Address: ", "got Response");
+            for(WifiP2pDevice wifiP2pDevice: wifiP2pDeviceCollection){
+                returnMACList.add(wifiP2pDevice.deviceAddress);
+                Log.v("MAC Address: ", wifiP2pDevice.deviceAddress);
+            }
+            cMacListener.MACReturn(returnMACList);
+        }
+    }
+
+
+    public interface MACListener{
+        void MACReturn(List<String> MACList);
+    }
+}
--- app/src/main/java/com/zerolabs/hey/MainListFragment.java
+++ app/src/main/java/com/zerolabs/hey/MainListFragment.java
@@ -5,19 +5,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.zerolabs.hey.DiscoveryHelper.WLANP2PDiscovery;
+
+import java.util.List;
 
 /**
  * Created by march_000 on 11.10.2014.
  */
 public class MainListFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        wlanp2PDiscovery = new WLANP2PDiscovery(getActivity());
+        wlanp2PDiscovery.Initialize();
+    }
 
     public MainListFragment() {
     }
+
+    Button button;
+    WLANP2PDiscovery wlanp2PDiscovery;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        button = (Button)rootView.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                wlanp2PDiscovery.discoverMACAdresses(new WLANP2PDiscovery.MACListener() {
+                    @Override
+                    public void MACReturn(List<String> MACList) {
+
+                    }
+                });
+            }
+        });
         return rootView;
     }
 }
